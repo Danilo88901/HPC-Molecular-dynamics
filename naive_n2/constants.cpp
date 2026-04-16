328d66a8b068ea1ec2fd1b5dac074c24c9d9c272
Danilo88901 <danya.kobal123456@gmail.com>
2026-04-16 15:34:29 +0200

    Add physical constants to constants.cpp

---
+++ naive_n2/constants.cpp
@@ -0,0 +1,9 @@
+#include "constants.h"
+#include <cmath>
+
+const double kb = 1.380649e-23;
+const double sigma = 3.405e-10;
+const double epsilon = 1.654e-21;
+const double m = 6.6335e-26;
+const double T = 80.0;
+const double v_std = sqrt(kb * T / m);
