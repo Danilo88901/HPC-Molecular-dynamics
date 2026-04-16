b4f4b5817d2885b03072dff71f983d352134be0a
Danilo88901 <danya.kobal123456@gmail.com>
2026-04-16 20:58:05 +0200

    Update constants.h

--- naive_n2/constants.h
+++ naive_n2/constants.h
@@ -5,4 +5,14 @@ constexpr double kb = 1.380649e-23;
 constexpr double sigma = 3.405e-10;
 constexpr double epsilon = 1.654e-21;
 constexpr double m = 6.6335e-26;
-constexpr double M_PI=3.1415926535;
+
+constexpr double T0 = 80.0; // не называй T (конфликт с функцией temperature)
+
+constexpr double r_cut = 2.5;
+constexpr double r_cut2 = (r_cut * sigma) * (r_cut * sigma);
+
+constexpr double invr2 = sigma * sigma / r_cut2;
+constexpr double invr6 = invr2 * invr2 * invr2;
+constexpr double invr12 = invr6 * invr6;
+
+constexpr double U_shift = 4 * epsilon * (invr12 - invr6);
