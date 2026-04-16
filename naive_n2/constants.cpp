b7ed76f402b03a2cc3521af4e58626042d7a0a1d
Danilo88901 <danya.kobal123456@gmail.com>
2026-04-16 15:52:05 +0200

    Add inverse cut-off distance calculations

--- naive_n2/constants.cpp
+++ naive_n2/constants.cpp
@@ -10,3 +10,9 @@ const double v_std = sqrt(kb * T / m);
 const doulbe M_PI=3.1415926535;
 const double r_cut = 2.5 * sigma;
 const double r_cut2 = r_cut * r_cut;
+
+double inv_r2_cut = sigma * sigma / r_cut2;
+double inv_r6_cut = inv_r2_cut * inv_r2_cut * inv_r2_cut;
+double inv_r12_cut = inv_r6_cut * inv_r6_cut;
+
+double U_shift = 4.0 * epsilon * (inv_r12_cut - inv_r6_cut)
