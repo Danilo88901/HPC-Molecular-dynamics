2501ac10cae0d30954ce396074b2f701dedcc25a
Danilo88901 <danya.kobal123456@gmail.com>
2026-04-16 15:50:50 +0200

    Update constants.cpp

--- naive_n2/constants.cpp
+++ naive_n2/constants.cpp
@@ -8,3 +8,5 @@ const double m = 6.6335e-26;
 const double T = 80.0;
 const double v_std = sqrt(kb * T / m);
 const doulbe M_PI=3.1415926535;
+const double r_cut = 2.5 * sigma;
+const double r_cut2 = r_cut * r_cut;
