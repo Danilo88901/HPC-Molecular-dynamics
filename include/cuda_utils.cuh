48faf527e1913f20fa3204cde72528a332af7bea
Danilo88901 <danya.kobal123456@gmail.com>
2026-05-05 22:49:51 +0200

    Update cuda_utils.cuh

--- include/cuda_utils.cuh
+++ include/cuda_utils.cuh
@@ -18,3 +18,28 @@ __device__ inline double atomicAddDouble(double* address, double val) {
 
     return __longlong_as_double(old);
 }
+
+__device__ inline  double atomicMaxDouble(double* address, double val) {
+	unsigned long long int* address_as_ull =
+		(unsigned long long int*)address;
+
+	unsigned long long int old = *address_as_ull;
+	unsigned long long int assumed;
+
+	do {
+		assumed = old;
+
+		double current = __longlong_as_double(assumed);
+
+		double new_val = fmax(val, current);
+
+		old = atomicCAS(
+			address_as_ull,
+			assumed,
+			__double_as_longlong(new_val)
+		);
+
+	} while (assumed != old);
+
+	return __longlong_as_double(old);
+}
